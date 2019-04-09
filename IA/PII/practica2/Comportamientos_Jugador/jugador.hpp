44eef8b388e4cb8b432918c5a7630f620f35b34f
Antonio <antoniogamiz10@gmail.com>
2019-04-09 11:29:22 +0200

    empiezo practica II

--- IA/PII/practica2/Comportamientos_Jugador/jugador.hpp
+++ IA/PII/practica2/Comportamientos_Jugador/jugador.hpp
@@ -5,51 +5,54 @@
 
 #include <list>
 
-struct estado {
+struct estado
+{
   int fila;
   int columna;
   int orientacion;
 };
 
-class ComportamientoJugador : public Comportamiento {
-  public:
-    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
-      // Inicializar Variables de Estado
-      fil = col = 99;
-      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
-      destino.fila = -1;
-      destino.columna = -1;
-      destino.orientacion = -1;
-    }
-    ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
-      // Inicializar Variables de Estado
-      fil = col = 99;
-      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
-      destino.fila = -1;
-      destino.columna = -1;
-      destino.orientacion = -1;
-    }
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
-    ~ComportamientoJugador(){}
+class ComportamientoJugador : public Comportamiento
+{
+public:
+  ComportamientoJugador(unsigned int size) : Comportamiento(size)
+  {
+    // Inicializar Variables de Estado
+    fil = col = 99;
+    brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+    destino.fila = -1;
+    destino.columna = -1;
+    destino.orientacion = -1;
+  }
+  ComportamientoJugador(std::vector<std::vector<unsigned char>> mapaR) : Comportamiento(mapaR)
+  {
+    // Inicializar Variables de Estado
+    fil = col = 99;
+    brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+    destino.fila = -1;
+    destino.columna = -1;
+    destino.orientacion = -1;
+  }
+  ComportamientoJugador(const ComportamientoJugador &comport) : Comportamiento(comport) {}
+  ~ComportamientoJugador() {}
 
-    Action think(Sensores sensores);
-    int interact(Action accion, int valor);
-    void VisualizaPlan(const estado &st, const list<Action> &plan);
-    ComportamientoJugador * clone(){return new ComportamientoJugador(*this);}
+  Action think(Sensores sensores);
+  int interact(Action accion, int valor);
+  void VisualizaPlan(const estado &st, const list<Action> &plan);
+  ComportamientoJugador *clone() { return new ComportamientoJugador(*this); }
 
-  private:
-    // Declarar Variables de Estado
-    int fil, col, brujula;
-    estado actual, destino;
-    list<Action> plan;
+private:
+  // Declarar Variables de Estado
+  int fil, col, brujula;
+  estado actual, destino;
+  list<Action> plan;
 
-    // Métodos privados de la clase
-    bool pathFinding(int level, const estado &origen, const estado &destino, list<Action> &plan);
-    bool pathFinding_Profundidad(const estado &origen, const estado &destino, list<Action> &plan);
+  // Métodos privados de la clase
+  bool pathFinding(int level, const estado &origen, const estado &destino, list<Action> &plan);
+  bool pathFinding_Profundidad(const estado &origen, const estado &destino, list<Action> &plan);
 
-    void PintaPlan(list<Action> plan);
-    bool HayObstaculoDelante(estado &st);
-
+  void PintaPlan(list<Action> plan);
+  bool HayObstaculoDelante(estado &st);
 };
 
 #endif
